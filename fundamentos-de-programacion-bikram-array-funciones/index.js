46d726e5d233eb60c8a64c68f78063dd2dce0948
adrianMartinezTeba <119281105+adrianMartinezTeba@users.noreply.github.com>
2022-11-30 23:46:58 +0100

    Add files via upload

---
+++ fundamentos-de-programacion-bikram-array-funciones/index.js
@@ -0,0 +1,14 @@
+/**parejas */
+let arrayVacio=[];
+let arrayNumeros=[0,1,2,3,4,5,6,7,8,9];
+let arrayNumerosPares=[0,2,4,6,8];
+let arrayBidimensional=[[0,1,2],['a','b','c',]];
+function suma(a,b) {
+    return a+b;
+}
+function potenciacion(a,b){
+    return a**b;
+}
+function separarPalabras() {
+    return
+}
